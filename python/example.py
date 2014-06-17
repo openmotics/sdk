0d89e382f4eaba39e16e01ffabe796f43952e050
fryckbos <frederick.ryckbosch@gmail.com>
2014-06-17 20:13:21 +0200

    Added python sdk.

---
+++ python/example.py
@@ -0,0 +1,21 @@
+import sdk
+
+
+# Test code for the gateway api.
+api = OpenMoticsApi("user", "pass", "localhost:8443", False)
+print api.get_output_status()
+
+
+# Test code for the cloud api.
+api = OpenMoticsCloudApi("user", "pass")
+
+def callback(msg):
+    print msg
+
+try:
+    print api.msg_loop([ OpenMoticsCloudApi.MSG_OUTPUT_CHANGE ], callback)
+except Exception as e:
+    traceback.print_exc()
+    #print "%s" % e.getMessage()
+
+
